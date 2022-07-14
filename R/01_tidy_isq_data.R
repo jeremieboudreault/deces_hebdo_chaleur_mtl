891aaa287a3f265031e1832f82218052ed20cfd8
jeremieboudreault <59902559+jeremieboudreault@users.noreply.github.com>
2022-07-14 11:50:34 -0400

    Finalized tidying weekly deaths from ISQ.

--- R/01_tidy_isq_data.R
+++ R/01_tidy_isq_data.R
@@ -191,3 +191,82 @@ data.table::fwrite(
     dec  = ","
 )
 
+
+# Weekly deaths by sex ---------------------------------------------------------
+
+
+# Downloaded here : https://statistique.quebec.ca/fr/document/
+#                   nombre-hebdomadaire-de-deces-au-quebec/tableau/
+#                   deces-par-semaine-selon-le-sexe-quebec
+
+# Load deaths by sex.
+wdeath_sex_raw <- data.table::setDT(openxlsx::read.xlsx(
+    xlsxFile = "data/isq/DecesSemaine_QC_Sexe.xlsx",
+    startRow = 7L
+))
+
+# Update columns names.
+colnames(wdeath_sex_raw) <- c("YEAR", "FLAG", "SEX", 1:53)
+
+# Melt the table.
+wdeath_sex <- data.table::melt.data.table(
+    data            = wdeath_sex_raw,
+    id.vars         = c("YEAR", "FLAG", "SEX"),
+    variable.name   = "WEEK",
+    value.name      = "N_DEATH",
+    variable.factor = FALSE
+)
+
+# Look at the available values.
+table(wdeath_sex$YEAR)
+table(wdeath_sex$FLAG)
+table(wdeath_sex$SEX)
+table(wdeath_sex$WEEK)
+
+# Overwrite the sex categories.
+wdeath_sex[, SEX := gsub("Femmes", "F", SEX)]
+wdeath_sex[, SEX := gsub("Hommes", "H", SEX)]
+
+# Convert <WEEK> to integer format.
+wdeath_sex[, WEEK := as.integer(as.character(WEEK))]
+wdeath_sex <- wdeath_sex[!is.na(WEEK), ]
+
+# Merge with dates.
+wdeath_sex <- data.table::merge.data.table(
+    x = wdeath_sex,
+    y = cdc_weeks,
+    by = c("YEAR", "WEEK"),
+    all.x = TRUE
+)
+
+# Remove empty dates and deaths.
+wdeath_sex <- wdeath_sex[!is.na(MID_DATE), ]
+wdeath_sex <- wdeath_sex[!is.na(N_DEATH), ]
+
+# Reorder columns.
+wdeath_sex <- wdeath_sex[, .(YEAR, WEEK, START_DATE, MID_DATE, END_DATE,
+                             SEX, N_DEATH, FLAG)]
+
+# Plot the results prior to save.
+ggplot(
+    data    = wdeath_sex[SEX != "Total"],
+    mapping = aes(x = MID_DATE, y = N_DEATH)
+) +
+geom_line(aes(color = SEX)) +
+ggtitle("Décès hebdomadaires au Québec par sexe") +
+labs(y = "Décès hebdomadaires", x = "Date") +
+scale_x_date(expand = expansion(mult = c(0.01, 0.02))) +
+scale_color_manual(values = c(jtheme::colors$blue, jtheme::colors$red)) +
+guides(colour = guide_legend(nrow = 1)) +
+jtheme(legend.title = FALSE)
+
+# Save plots.
+jtheme::save_ggplot("plots/fig_3_deces_par_sexe.jpg", size = "rect")
+
+# Save dataset.
+data.table::fwrite(
+    x    = wdeath_region,
+    file = "data/cleaned/wdeaths_sex.csv",
+    sep  = ";",
+    dec  = ","
+)
