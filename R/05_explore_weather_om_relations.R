1bbb387befbdd2658ccd1ef3f7e746ee131a4a5d
jeremieboudreault <59902559+jeremieboudreault@users.noreply.github.com>
2022-07-15 13:01:53 -0400

    Initial commit on a function to generate a non-linear relation between temperature metric and mortality/over-mortality metric.

--- R/05_explore_weather_om_relations.R
+++ R/05_explore_weather_om_relations.R
@@ -17,6 +17,7 @@
 library(corrplot)
 library(data.table)
 library(ggplot2)
+library(ggpubr)
 library(jtheme)
 
 
@@ -82,4 +83,68 @@ mcor_summer <- cor(data[WEEK > 16 & WEEK < 38, .(
 pdf("plots/fig_7_2_cor_mat_ete.pdf", width = 7, height = 5)
 corrplot_full(mcor_summer, "Matrice de corrélation (mai-septembre)")
 dev.off()
+
+
+# Generate a plot of the relation between temperature and mortality ------------
+
+
+generate_plot <- function(
+    plot_i       = 1L,
+    week_start   = 1L,
+    week_end     = 53L,
+    year_start   = 2010,
+    year_end     = 2019,
+    temp_metric  = "TEMP_MEAN_MEAN",
+    death_metric = "N_DEATH"
+){
+
+    # Subset data.
+    data_sub <- data[YEAR >= year_start &
+                     YEAR <= year_end   &
+                     WEEK >= week_start &
+                     WEEK <= week_end, ]
+    data_sub$TEMP_METRIC  <- data_sub[[temp_metric]]
+    data_sub$DEATH_METRIC <- data_sub[[death_metric]]
+
+    # Name of the death variable.
+    death_name   <- if (death_metric == "N_DEATH") {
+        "Mortalité"
+    } else {
+        "Surmoralité"
+    }
+
+    # Period name.
+    period_name  <- if (week_start == 1L) {
+        "Janvier à décembre"
+    } else {
+        "Mai à septembre"
+    }
+
+    # Period name 2.
+    period_name2 <- if (week_start == 1L) {
+        "annuelle"
+    } else {
+        "estivale"
+    }
+
+    # Temperature
+    temp_name    <- if (temp_metric == "TEMP_MEAN_MEAN") {
+        "Moyenne hebdomadaire des Tmoy"
+    } else if (temp_metric == "TEMP_MAX_MEAN") {
+        "Moyenne hebdomadaire des Tmax"
+    }
+
+    # Plot
+    ggplot(data_sub, aes(x = TEMP_METRIC, y = DEATH_METRIC)) +
+        geom_jitter(aes(color = TEMP_METRIC)) +
+        geom_smooth(color = "black") +
+        scale_color_distiller(palette = "YlOrRd", direction = 1, limits = c(-20, 35)) +
+        ggtitle(paste(death_name, period_name2), paste0(period_name, " (", year_start, "-", year_end, ")")) +
+        labs(
+            y = death_name,
+            x = ifelse(plot_i %in% c(3L, 4L), temp_name, "")
+        ) +
+        jtheme(legend.title = FALSE, facets = TRUE)
+
+}
 
