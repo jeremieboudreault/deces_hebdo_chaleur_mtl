8b6412f60e3b7d8241c823286b133943ab8ea681
Jeremie Boudreault <jeremieboudreault11@gmail.com>
2022-07-26 15:14:53 -0400

    Updated extract_eccc station to keep only station with valid hourly data.

--- R/02_extract_eccc_stns_by_rss.R
+++ R/02_extract_eccc_stns_by_rss.R
@@ -35,13 +35,39 @@ rss <- sf::read_sf("data/rss/Territoires_RSS_2022.shp")
 # Load list of stations
 stns <- data.table::data.table(weathercan::stations())
 
-# Keep only stations in QC with hourly data and long/lat.
+# Keep only stations in QC, with hourly data and valid long/lat.
 stns <- stns[
     prov     == "QC"   &
     interval == "hour" &
     !is.na(lat)        &
-    !is.na(lon)
+    !is.na(lon)        &
+    !is.na(start)      &
+    !is.na(end)
 ]
+
+
+# Leaflet map of all stations with hourly data ---------------------------------
+
+
+leaflet::leaflet(
+) |>
+leaflet::addProviderTiles(
+    provider = leaflet::providers$CartoDB.Voyager
+) |>
+leaflet::addCircleMarkers(
+    data        = stns,
+    lng         = ~ lon,
+    lat         = ~ lat,
+    radius      = 4L,
+    stroke      = TRUE,
+    color       = "black",
+    weight      = 1.2,
+    opacity     = 0.8,
+    fillColor   = "orange",
+    fillOpacity = 1L,
+    label       = ~ station_name,
+    clusterOptions = leaflet::markerClusterOptions()
+)
 
 
 # Keep only stations in RSS of Montreal and Laval ------------------------------
@@ -71,14 +97,14 @@ stns_sf$RSS_CODE <- rss$RSS_code[unlist(inter)]
 # Keep stations that do not have a NA.
 stns <- stns[!is.na(RSS_CODE), ]
 stns_sf <- stns_sf[!is.na(stns_sf$RSS_CODE), ]
-nrow(stns_qc) == nrow(stns_sf)
+nrow(stns) == nrow(stns_sf)
 
 # Plot final list of stations by RSS.
 plot(stns_sf["RSS_CODE"])
 
 # Keep only stations in Montreal and Laval.
 stns_mtl <- stns[RSS_CODE %in% c("06", "13"), ]
-stns_sf_mtl <- stns_sf[stns_qc_sf$RSS_CODE %in% c("06", "13"), ]
+stns_sf_mtl <- stns_sf[stns_sf$RSS_CODE %in% c("06", "13"), ]
 nrow(stns_mtl) == nrow(stns_sf_mtl)
 
 
@@ -110,9 +136,11 @@ jtheme::save_ggplot("plots/fig_4_carte_stations.jpg", size = "rect")
 
 
 # Stations list.
-qs::qsave(
+data.table::fwrite(
     x    = stns_mtl,
-    file = "data/eccc/stns_mtl_list.qs"
+    file = "data/eccc/stns_mtl_list.csv",
+    dec  = ",",
+    sep  = ";"
 )
 
 
