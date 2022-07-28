84b2d432854f101c3dbd44469ac5497c70f0a467
Jeremie Boudreault <jeremieboudreault11@gmail.com>
2022-07-28 14:56:29 -0400

    Updated script 3 to download hourly data using the REST API of ECCC directly instead of waethercan R package that failed for hourly data.

--- R/03_download_eccc_data.R
+++ R/03_download_eccc_data.R
@@ -1,7 +1,7 @@
 # 03_download_eccc_data.R
 
 
-# Step 03 : Download data from ECCC given the station list.
+# Step 03 : Download raw hourly data from ECCC given the station list.
 
 
 # Project : deces_isq_chaleur
@@ -18,7 +18,6 @@
 library(data.table)
 library(ggplot2)
 library(jtheme)
-library(weathercan)
 
 
 # Imports ----------------------------------------------------------------------
@@ -27,33 +26,58 @@ library(weathercan)
 # List of stations.
 stns_list <- data.table::fread("data/eccc/stns_mtl_list.csv", dec = ",")
 
+# Keep stations that have data after 1979.
+stns_list <- stns_list[end >= 1979L, ]
 
-# Download data for the Monteal stations ---------------------------------------
 
+# Download data for the stations in Monteal/Laval ------------------------------
 
-# Note : This step is very long.
 
 # Download from ECCC by looping on all stations.
-for (stn_id in stns_list$station_id) {
+for (stn_i in seq_len(nrow(stns_list))) {
 
-    # Download data from ECCC.
-    weather_data_tmp <- weathercan::weather_dl(
-        station_ids = stn_id,
-        interval    = "hour",
-        start       = "1979-01-01",
-        verbose     = TRUE
-    )
+    # Extract station id
+    stn_id <- stns_list[stn_i, station_id]
 
-    # Save file if not empty.
-    if (nrow(weather_data_tmp) > 0) {
-        data.table::fwrite(
-            x     = weather_data_tmp,
-            file  = paste0("data/eccc/raw/", stn_id, ".csv"),
-            sep   = ";",
-            dec   = ","
-        )
+    # Extract start and end year.
+    start <- max(stns_list[stn_i, start], 1979L)
+    end   <- stns_list[stn_i, end]
+
+    # Loop on years.
+    for (year in start:end) {
+
+        # Loop on months.
+        for (month in 1:12) {
+
+            # Message.
+            message(
+                "Download hourly data for station ", stn_id, ", ",
+                year, "/", month, "."
+            )
+
+            # Create URL.
+            url <- paste0(
+                "https://climate.weather.gc.ca/climate_data/bulk_data_e.html?",
+                "format=csv&stationID=", stn_id, "&Year=", year, "&Month=", month,
+                "&Day=14&timeframe=1&submit=%20Download+Data"
+            )
+
+            # Filename.
+            filename <- paste0(stn_id, "_", year, "_", month, ".csv")
+
+            # Download file.
+            download.file(
+                url      = url,
+                destfile = file.path("data", "eccc", "raw", filename),
+                quiet    = TRUE
+            )
+
+            # End of loop for month.
+        }
+
+        # End of loop for year.
     }
 
-# End of the loop.
+    # End of loop for season.
 }
 
