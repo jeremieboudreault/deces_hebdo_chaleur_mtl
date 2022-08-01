7f7baf8da3185e840bb72a786de076f87583f5cd
Jeremie Boudreault <jeremieboudreault11@gmail.com>
2022-08-01 16:04:30 -0400

    Added all weather metrics to the weekly file.

--- R/05_merge_death_weather.R
+++ R/05_merge_death_weather.R
@@ -29,7 +29,7 @@ deaths_mtl <- data.table::fread("data/isq/wdeaths_region.csv")
 deaths_mtl <- deaths_mtl[REGION == "Montréal et Laval", ]
 
 # Temperature values in Montreal.
-weather_mtl <- qs::qread("data/eccc/mtl_stns_data_agg.qs")
+weather_mtl <- qs::qread("data/eccc/mtl_data_daily_agg.qs")
 
 
 # Expand CDC week to get a data.table ------------------------------------------
@@ -63,23 +63,28 @@ data.table::fwrite(
 weather_w_week <- data.table::merge.data.table(
     x     = cdc_weeks_dates,
     y     = weather_mtl,
-    by.y  = "date",
-    by.x  = "DATE",
+    by    = c("DATE", "YEAR"),
     all.x = TRUE
 )
 
 # Remove missing.
-weather_w_week <- weather_w_week[!is.na(mean_temp), ]
+weather_w_week <- weather_w_week[!is.na(T_MEAN), ]
 
 # Summarize with weekly temperature values.
 weather_weekly <- weather_w_week[, .(
-    TEMP_MIN_MIN   = round(min(min_temp,   na.rm = TRUE), 2L),
-    TEMP_MIN_MEAN  = round(mean(min_temp,  na.rm = TRUE), 2L),
-    TEMP_MEAN_MIN  = round(min(mean_temp,  na.rm = TRUE), 2L),
-    TEMP_MEAN_MEAN = round(mean(mean_temp, na.rm = TRUE), 2L),
-    TEMP_MEAN_MAX  = round(max(mean_temp,  na.rm = TRUE), 2L),
-    TEMP_MAX_MEAN  = round(mean(max_temp,  na.rm = TRUE), 2L),
-    TEMP_MAX_MAX   = round(max(max_temp,   na.rm = TRUE), 2L)
+    T_MIN_WMEAN      = round(mean(T_MIN,      na.rm = TRUE), 2L),
+    T_MEAN_WMEAN     = round(mean(T_MEAN,     na.rm = TRUE), 2L),
+    T_MAX_WMEAN      = round(mean(T_MAX,      na.rm = TRUE), 2L),
+    HMDX_MIN_WMEAN   = round(mean(HMDX_MIN,   na.rm = TRUE), 2L),
+    HMDX_MEAN_WMEAN  = round(mean(HMDX_MEAN,  na.rm = TRUE), 2L),
+    HMDX_MAX_WMEAN   = round(mean(HMDX_MAX,   na.rm = TRUE), 2L),
+    TDEW_MEAN_WMEAN  = round(mean(TDEW_MEAN,  na.rm = TRUE), 2L),
+    WDCHL_MEAN_WMEAN = round(mean(WDCHL_MEAN, na.rm = TRUE), 2L),
+    RELH_MEAN_WMEAN  = round(mean(RELH_MEAN,  na.rm = TRUE), 2L),
+    PRES_MEAN_WMEAN  = round(mean(PRES_MEAN,  na.rm = TRUE), 2L),
+    WDSPD_MEAN_WMEAN = round(mean(WDSPD_MEAN, na.rm = TRUE), 2L),
+    PRCIP_SUM_WSUM   = round(sum(PRCIP_SUM,   na.rm = TRUE), 2L),
+    VISB_MEAN_WMEAN  = round(mean(VISB_MEAN,  na.rm = TRUE), 2L)
 ), by = c("YEAR", "WEEK")]
 
 
@@ -102,12 +107,25 @@ weather_death_weekly <- weather_death_weekly[!is.na(N_DEATH), ]
 
 
 table_final <- weather_death_weekly[, .(
-    YEAR, WEEK,
-    START_DATE, MID_DATE, END_DATE,
+    YEAR,
+    WEEK,
+    START_DATE,
+    MID_DATE,
+    END_DATE,
     N_DEATH,
-    TEMP_MIN_MIN, TEMP_MIN_MEAN,
-    TEMP_MEAN_MIN, TEMP_MEAN_MEAN, TEMP_MEAN_MAX,
-    TEMP_MAX_MEAN, TEMP_MAX_MAX
+    T_MIN_WMEAN,
+    T_MEAN_WMEAN,
+    T_MAX_WMEAN,
+    HMDX_MIN_WMEAN,
+    HMDX_MEAN_WMEAN,
+    HMDX_MAX_WMEAN,
+    TDEW_MEAN_WMEAN,
+    WDCHL_MEAN_WMEAN,
+    RELH_MEAN_WMEAN,
+    PRES_MEAN_WMEAN,
+    WDSPD_MEAN_WMEAN,
+    PRCIP_SUM_WSUM,
+    VISB_MEAN_WMEAN
 )]
 
 
